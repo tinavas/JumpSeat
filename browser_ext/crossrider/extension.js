d173f5049a0ab875dcac538016c5bdc01f7dada3
mikepriest <mike.priest@10spd.com>
2015-10-27 22:15:50 -0600

    - Added handler for enter key press

--- browser_ext/crossrider/extension.js
+++ browser_ext/crossrider/extension.js
@@ -1,6 +1,6 @@
 appAPI.ready(function($) {
     var hostname = appAPI.db.get('hostname');
-    alert(hostname);
+    alert("tset");
     appAPI.dom.addRemoteJS({
         url: hostname + '/aerospace',
         additionalAttributes: { charset: "UTF-8" },
--- browser_ext/extension-aio/resources/popup.js
+++ browser_ext/extension-aio/resources/popup.js
@@ -2,3 +2,11 @@ document.getElementById('save').onclick = function (e) {
     appAPI.db.set('hostname', document.getElementById('hostname').value);
     appAPI.browserAction.closePopup();
 }
+
+document.getElementById('hostname').onkeyup = function(e){
+    if (e.keyCode == 13) {
+        appAPI.db.set('hostname', document.getElementById('hostname').value);
+        appAPI.browserAction.closePopup();
+        return false;
+    }
+}
