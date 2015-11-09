cc75e9bbc11a469b996d5751c13fcffda9a81fc8
mikepriest <mike.priest@10spd.com>
2015-11-09 17:35:20 +0000

    - Added login button to saved plugin settings.

--- browser_ext/extension-aio/resources/popup.js
+++ browser_ext/extension-aio/resources/popup.js
@@ -1,12 +1,39 @@
-document.getElementById('save').onclick = function (e) {
-    appAPI.db.set('hostname', document.getElementById('hostname').value);
-    appAPI.browserAction.closePopup();
-}
+jQuery(function(){
+    (function($){
 
-document.getElementById('hostname').onkeyup = function(e){
-    if (e.keyCode == 13) {
-        appAPI.db.set('hostname', document.getElementById('hostname').value);
-        appAPI.browserAction.closePopup();
-        return false;
-    }
-}
+        //Setup Login Button
+        var hostname = appAPI.db.get('hostname');
+        if(hostname && hostname !== ""){
+            $('button').addClass('btn-half');
+            $('#login').show();
+        }
+
+        //On save
+        $('#login').on('click', function(){
+            var win = window.open($('#hostname').val(), '_blank');
+            if(win){
+                //Browser has allowed it to be opened
+                win.focus();
+            }else{
+                //Broswer has blocked it
+                alert('Please allow popups for this site');
+            }
+        });
+
+        //On save
+        $('#save').on('click', function(){
+            appAPI.db.set('hostname', $('#hostname').val());
+            appAPI.browserAction.closePopup();
+        });
+
+        //On enter key
+        $('#hostname').on('keyup', function(e){
+            if(e.which == 13) {
+                appAPI.db.set('hostname', $('#hostname').val());
+                appAPI.browserAction.closePopup();
+                return false;
+            }
+        });
+
+    })(jQuery);
+});
