6614d043b5664beb200a4e9dba5457e2934a006d
mikepriest <mike.priest@10spd.com>
2015-11-12 14:20:08 +0000

    - Remove timeout dependency

--- assets/js/aero/user/_main.js
+++ assets/js/aero/user/_main.js
@@ -6,21 +6,19 @@
  */
 function AeroMain() {
 
-    //Start the crazies; requires timeout FF
+    //Start the crazies
     $q(function(){
-        setTimeout(function(){
-            //Cross domain check
-            aeroStorage.getItem('aero:sidebar:cdshost', function(bits){
-                if(bits){
-                    var bitsArr = bits.split("~");
+        //Cross domain check
+        aeroStorage.getItem('aero:sidebar:cdshost', function(bits){
+            if(bits){
+                var bitsArr = bits.split("~");
 
-                    if(bitsArr[1] != window.location.host){
-                        aeroStorage.setItem("aero:session:pause", bitsArr[0]);
-                    }
+                if(bitsArr[1] != window.location.host){
+                    aeroStorage.setItem("aero:session:pause", bitsArr[0]);
                 }
-                Aero.guide.init();
-            }, true);
-        }, 100);
+            }
+            Aero.guide.init();
+        }, true);
     });
 
     //Initialize Pathways
